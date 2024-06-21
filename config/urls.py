3b209f87e790e1ec5bba7165eb4697714c507681
Samirxon <100518687+mir7ayev@users.noreply.github.com>
2024-06-21 19:45:07 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import re_path, path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,7 +10,14 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('contact/', include('contact.urls')),
     path('auth/', include('authentication.urls')),
+
+    re_path(r'^static/(?P<path>.*)$', serve),
+    re_path(
+            r'^media/(?P<path>.*)$', serve,
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}
+        ),
 ]
+
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
